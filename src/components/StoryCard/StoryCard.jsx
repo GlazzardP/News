b82f8231eaba9242ae3007e6a99a2ae659271f8f
Paul Glazzard <Paul.Glazzard@Nology.local>
2020-03-30 19:30:06 +0100

    re-arranged storyCard

--- src/components/StoryCard/StoryCard.jsx
+++ src/components/StoryCard/StoryCard.jsx
@@ -11,18 +11,23 @@ const StoryCard = props => {
   const { news, updateFavourites } = props;
 
   const insertJsx = news.map(article => (
-    <div className={styles.newsStory}>
-      <h3>{article.title}</h3>
-      <p>{article.description}</p>
-      <a href={article.url}>Link to article</a>
-      {/* <p>Link to article: {article.url}</p> */}
+    <div className={styles.newsStory} href={article.url}>
       <img src={article.urlToImage} alt="test" />
-      <FontAwesomeIcon icon={faTimesCircle} className={styles.Awesome} />
-      <FontAwesomeIcon icon={faCheckCircle} />
-      <FontAwesomeIcon
-        icon={faBookmark}
-        onClick={() => updateFavourites(article)}
-      />
+
+      <h3>{article.title}</h3>
+      {/* <p>{article.description}</p> */}
+      {/* <a href={article.url}>Link to article</a> */}
+      {/* <p>Link to article: {article.url}</p> */}
+      <div className={styles.favIcons}>
+        <FontAwesomeIcon icon={faTimesCircle} className={styles.Awesome} />
+        <FontAwesomeIcon icon={faCheckCircle} className={styles.Awesome} />
+        <FontAwesomeIcon
+          icon={faBookmark}
+          onClick={() => updateFavourites(article)}
+          className={styles.Awesome}
+        />
+      </div>
+
       {/* <span class=" fa fas fa-check"></span>
       <span class=" fa fas fa-times"></span> */}
     </div>
